f555f2abac59a0e721deb0eb1810e1945974f80b
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-02 20:11:27 +0530

    Create 22_Generate_Parentheses.cpp

---
+++ Medium/22_Generate_Parentheses.cpp
@@ -0,0 +1,35 @@
+// Problem: Generate Parentheses
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/generate-parentheses/
+// Approach: Backtracking
+// Time Complexity: O(4^n / √n)
+// Space Complexity: O(n) (excluding output)
+
+class Solution {
+public:
+    void solve(int open, int close,
+               string s,
+               vector<string>& ans) {
+
+        if (open == 0 && close == 0) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open > 0) {
+            solve(open - 1, close, s + '(', ans);
+        }
+
+        if (close > open) {
+            solve(open, close - 1, s + ')', ans);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+
+        solve(n, n, "", ans);
+
+        return ans;
+    }
+};
