41fe2a7fe592119c33394c379710d548817f0d33
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 10:29:56 +0530

    Create 1700_Number_of_Students_Unable_to_Eat_Lunch.cpp

---
+++ Easy/1700_Number_of_Students_Unable_to_Eat_Lunch.cpp
@@ -0,0 +1,33 @@
+// Problem: Number of Students Unable to Eat Lunch
+// Difficulty: Easy
+// Approach: Counting
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int zero = 0, one = 0;
+
+        for (int s : students) {
+            if (s == 0)
+                zero++;
+            else
+                one++;
+        }
+
+        for (int s : sandwiches) {
+            if (s == 0) {
+                if (zero == 0)
+                    return one;
+                zero--;
+            } else {
+                if (one == 0)
+                    return zero;
+                one--;
+            }
+        }
+
+        return 0;
+    }
+};
