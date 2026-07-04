c9f7150315554d87fb429f15a959d1858306f164
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 11:53:42 +0530

    Add string processing solution with special operations
    Implement string processing with special operations like duplication, reversal, and character removal.

---
+++ Medium/Process_String_With_Special_Operations.cpp
@@ -0,0 +1,33 @@
+// Problem: Process String With Special Operations
+// Difficulty: Medium
+// Approach: String Simulation
+// Time Complexity: O(n * k) (due to reverse/duplication ops)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = s[i];
+
+            if (islower(ch)) {
+                ans.push_back(ch);
+            }
+            else if (ch == '*' && !ans.empty()) {
+                ans.pop_back();
+            }
+            else if (ch == '#') {
+                ans += ans;
+            }
+            else if (ch == '%') {
+                reverse(ans.begin(), ans.end());
+            }
+        }
+
+        return ans;
+    }
+};
