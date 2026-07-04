8a500fb1ecb74e5ea3f0b1471e5a08984249556f
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 11:52:25 +0530

    Create 8_String_to_Integer_atoi.cpp

---
+++ Medium/8_String_to_Integer_atoi.cpp
@@ -0,0 +1,38 @@
+// Problem: String to Integer (atoi)
+// Difficulty: Medium
+// Approach: Simulation + Overflow Handling
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        int sign = 1;
+        long long num = 0;
+
+        while (i < n && s[i] == ' ')
+            i++;
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            num = num * 10 + digit;
+
+            if (sign == 1 && num > INT_MAX)
+                return INT_MAX;
+
+            if (sign == -1 && -num < INT_MIN)
+                return INT_MIN;
+
+            i++;
+        }
+
+        return (int)(sign * num);
+    }
+};
