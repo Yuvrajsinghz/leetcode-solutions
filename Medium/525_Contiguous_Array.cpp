4db7ef4ac5cb173608da9b87ac20022a941214bd
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 12:00:04 +0530

    Create 525_Contiguous_Array.cpp

---
+++ Medium/525_Contiguous_Array.cpp
@@ -0,0 +1,36 @@
+// Problem: Contiguous Array
+// Difficulty: Medium
+// Approach: Prefix Sum + Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        mp[0] = -1;
+
+        int sum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] == 0) {
+                sum -= 1;
+            }
+            else {
+                sum += 1;
+            }
+
+            if (mp.find(sum) != mp.end()) {
+                ans = max(ans, i - mp[sum]);
+            }
+            else {
+                mp[sum] = i;
+            }
+        }
+
+        return ans;
+    }
+};
