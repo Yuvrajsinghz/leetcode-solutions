d5ef47bbdc6e8e1e92c1715d24be05a9031dc4a9
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 11:00:15 +0530

    Create 64_Minimum_Path_Sum.cpp

---
+++ Medium/64_Minimum_Path_Sum.cpp
@@ -0,0 +1,29 @@
+// Problem: Minimum Path Sum
+// Difficulty: Medium
+// Approach: Dynamic Programming (In-Place DP)
+// Time Complexity: O(m * n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+
+        for (int j = 1; j < m; j++) {
+            grid[j][0] += grid[j - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
