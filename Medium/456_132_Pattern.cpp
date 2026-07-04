ec8e8d23680f012e622fa7dccd7e7a22aa325b16
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 11:42:10 +0530

    Create 456_132_Pattern.cpp

---
+++ Medium/456_132_Pattern.cpp
@@ -0,0 +1,31 @@
+// Problem: 132 Pattern
+// Difficulty: Medium
+// Approach: Monotonic Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+
+        stack<int> st;
+        int third = INT_MIN;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (third > nums[i]) {
+                return true;
+            }
+
+            while (!st.empty() && st.top() < nums[i]) {
+                third = st.top();
+                st.pop();
+            }
+
+            st.push(nums[i]);
+        }
+
+        return false;
+    }
+};
