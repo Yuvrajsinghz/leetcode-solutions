85c3cb4a8383e99c5d873babb19070d6d3af3af4
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-04 11:15:58 +0530

    Add Two Sum solution using hash map
    Implemented a solution for the Two Sum problem using a hash map for efficient lookups.

---
+++ Easy/1_Two_Sum.cpp
@@ -0,0 +1,24 @@
+// Problem: Two Sum
+// Difficulty: Easy
+// Approach: Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
