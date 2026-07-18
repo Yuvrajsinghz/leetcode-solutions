5e18755b05624cf3b83b7861d99714d4734b3777
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-18 10:20:43 +0530

    Create 3760_GCD_Sum_Pairing.cpp

---
+++ Medium/3760_GCD_Sum_Pairing.cpp
@@ -0,0 +1,31 @@
+// Problem: GCD Sum Pairing
+// Difficulty: Medium
+// Approach: Prefix GCD + Sorting + Pairing
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> prefixGcd(n);
+
+        int mx = 0;
+
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+            prefixGcd[i] = gcd(nums[i], mx);
+        }
+
+        sort(prefixGcd.begin(), prefixGcd.end());
+
+        long long ans = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            ans += gcd(prefixGcd[i], prefixGcd[n - 1 - i]);
+        }
+
+        return ans;
+    }
+};
