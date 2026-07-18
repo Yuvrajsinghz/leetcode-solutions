58e8982b734b7b054d26c150462f1f5a0fd23f72
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-18 10:18:33 +0530

    Create 3658_GCD_of_Odd_and_Even_Sums.cpp

---
+++ Easy/3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,12 @@
+// Problem: GCD of Odd and Even Sums
+// Difficulty: Easy
+// Approach: Mathematical Observation
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
