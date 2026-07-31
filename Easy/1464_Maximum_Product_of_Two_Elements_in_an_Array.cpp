8dbacf21deea8a4176e27cf5f02a3379ebe0754a
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-07-31 22:27:43 +0530

    Create 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp

---
+++ Easy/1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -0,0 +1,27 @@
+// Problem: Maximum Product of Two Elements in an Array
+// Difficulty: Easy
+// Approach: Track Two Largest Elements
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+        int max1 = 0;
+        int max2 = 0;
+
+        for (int num : nums) {
+
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+};
