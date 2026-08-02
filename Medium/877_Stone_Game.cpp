8c7b228e1517b1293abfac6e9ef0a26d992efa86
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-08-02 09:14:42 +0530

    Create 877_Stone_Game.cpp

---
+++ Medium/877_Stone_Game.cpp
@@ -0,0 +1,12 @@
+// Problem: Stone Game
+// Difficulty: Medium
+// Approach: Mathematical Observation
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
