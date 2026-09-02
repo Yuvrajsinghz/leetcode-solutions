04c481908a8c4cf3bba8fa9d9c3abaeebd977160
Yuvraj Singh Rathore <thakuryuvrajrathore@gmail.com>
2026-09-02 08:05:43 +0530

    Create 3875_Construct_Uniform_Parity_Array_I.cpp

---
+++ Easy/3875_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,13 @@
+// Problem: Construct Uniform Parity Array I
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Approach: Check whether the array can be made to have uniform parity.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
